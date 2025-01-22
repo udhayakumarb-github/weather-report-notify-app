a2922b0ffb8cb97c563e8a2a9be41aef8ec24e8d
Udhayakumar Baskaran <udhayse@gmail.com>
2025-01-23 00:04:07 +0530

    notify app

--- src/main/scala/com/elan/weather/WeatherMainApp.scala
+++ src/main/scala/com/elan/weather/WeatherMainApp.scala
@@ -3,8 +3,6 @@ package com.elan.weather
 import akka.actor.typed.ActorSystem
 import com.elan.weather.consumers.WeatherConsumer
 import org.slf4j.LoggerFactory
-import com.typesafe.config.ConfigFactory
-import slick.jdbc.PostgresProfile.api._
 
 object Main extends App {
   private val logger = LoggerFactory.getLogger(this.getClass)
